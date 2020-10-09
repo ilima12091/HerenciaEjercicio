cf170997da8007d1085324bd460af4270ada765c
ilima12091 <limaignacio12091@gmail.com>
2020-10-09 17:56:58 -0300

    Agrego algunos comentarios

--- src/Library/IPoster.cs
+++ src/Library/IPoster.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 namespace PII_Herencia
 {
+    /*
+        Creamos la interface IPoster para poder hacer que nuestro programa pueda crecer y expandirse a postear en otras plataformas
+        además de twitter.
+    */
     public interface IPoster
     {
         void Add(Usuario usuario);
